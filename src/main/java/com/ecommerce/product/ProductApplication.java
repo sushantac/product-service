bf436cef71bed925d67bea1ea8dc0b1bd38e8d39
Sushant <sushantac@gmail.com>
2026-09-11 22:07:59 +1000

    fix(config): disable Kafka auto-configuration

--- src/main/java/com/ecommerce/product/ProductApplication.java
+++ src/main/java/com/ecommerce/product/ProductApplication.java
@@ -3,7 +3,7 @@ package com.ecommerce.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = KafkaAutoConfiguration.class)
 public class ProductApplication {
 
     public static void main(String[] args) {
