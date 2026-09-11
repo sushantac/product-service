39ac59c0f4571047da528bd04907aafac23fc353
Sushant <sushantac@gmail.com>
2026-09-11 22:22:03 +1000

    fix(config): fix KafkaAutoConfiguration import

--- src/main/java/com/ecommerce/product/ProductApplication.java
+++ src/main/java/com/ecommerce/product/ProductApplication.java
@@ -2,6 +2,7 @@ package com.ecommerce.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 
 @SpringBootApplication(exclude = KafkaAutoConfiguration.class)
 public class ProductApplication {
