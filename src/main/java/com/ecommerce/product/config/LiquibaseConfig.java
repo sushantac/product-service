839760dedec274401b6b05197d39ef40fa89c4be
Sushant <sushantac@gmail.com>
2026-09-11 15:09:28 +1000

    chore(liquibase): fix exception handling

--- src/main/java/com/ecommerce/product/config/LiquibaseConfig.java
+++ src/main/java/com/ecommerce/product/config/LiquibaseConfig.java
@@ -1,12 +1,14 @@
 package com.ecommerce.product.config;
 
-import javax.sql.DataSource;
+import liquibase.exception.LiquibaseException;
 import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class LiquibaseConfig {
@@ -19,15 +21,19 @@ public class LiquibaseConfig {
         SpringLiquibase liquibase = new SpringLiquibase() {
             @Override
             public void afterPropertiesSet() {
-                org.springframework.jdbc.core.JdbcTemplate jdbcTemplate = new org.springframework.jdbc.core.JdbcTemplate(dataSource);
-                jdbcTemplate.execute("CREATE SCHEMA IF NOT EXISTS " + schema);
-                super.afterPropertiesSet();
+                try {
+                    org.springframework.jdbc.core.JdbcTemplate jdbcTemplate = new org.springframework.jdbc.core.JdbcTemplate(dataSource);
+                    jdbcTemplate.execute("CREATE product IF NOT EXISTS " + schema);
+                    super.afterPropertiesSet();
+                } catch (liquibase.exception.LiquibaseException e) {
+                    throw new IllegalStateException("Failed to initialize Liquibase", e);
+                }
             }
         };
         liquibase.setDataSource(dataSource);
         liquibase.setChangeLog(properties.getChangeLog());
         liquibase.setDefaultSchema(schema);
-        liquibase.setContexts(properties.getContexts());
+        liquibase.setContexts(properties.getContexts() != null ? String.join(",", properties.getContexts()) : "");
         liquibase.setDropFirst(properties.isDropFirst());
         return liquibase;
     }
